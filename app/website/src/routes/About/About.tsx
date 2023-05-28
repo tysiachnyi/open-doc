731cd6658c111c6b2ba72973b1518615631d1dd2
Nik <nikita.tysiachnyi@gmail.com>
2023-05-28 17:30:59 +0200

    Updated Home and About page

--- app/website/src/routes/About/About.tsx
+++ app/website/src/routes/About/About.tsx
@@ -1,8 +1,46 @@
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>This is the about page.</p>
+    <div className="bg-slate-100 flex items-center justify-center p-10">
+      <div className="text-gray-800 text-2xl">
+        <h1 className="mb-4">About Me</h1>
+        <p className="mb-6">
+          I'm Nik Tysiachnyi, a full-stack developer, and I'm thrilled to
+          introduce you to my open-source project! 🚀
+        </p>
+        <p className="mb-6">
+          As a passionate developer, I believe in the power of collaboration and
+          the impact of open-source software. That's why I've dedicated my time
+          and expertise to create this project, aiming to empower developers and
+          users alike. 💪
+        </p>
+        <p>
+          With this open-source project, I've combined my skills in front-end
+          and back-end development to deliver a comprehensive solution. 🌐
+          Whether you're looking for a sleek user interface or a robust backend
+          architecture, this project has got you covered! 💻
+        </p>
+        <p>
+          But this project isn't just about code. It's about fostering a
+          community of like-minded individuals who are passionate about
+          innovation and making a difference. Together, we can push the
+          boundaries of technology and create something truly remarkable. 🌟
+        </p>
+        <p>
+          So, join me on this exciting journey as we explore the limitless
+          possibilities of this open-source project. Let's collaborate, learn
+          from each other, and make a positive impact on the world of software
+          development! 🎉
+        </p>
+        <p>
+          Feel free to get involved, contribute, or reach out to me if you have
+          any questions or ideas. Together, we can take this project to new
+          heights! 🚀
+        </p>
+        <p>
+          Thank you for your support and enthusiasm! Let's build something
+          extraordinary together! 💪✨
+        </p>
+      </div>
     </div>
   );
 };
